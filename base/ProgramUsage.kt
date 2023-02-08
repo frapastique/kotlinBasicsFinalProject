595f96bc8491dff6b78eb244b4fbda619c93ae8c
frapastique <frapastique@outlook.com>
2023-02-08 11:49:45 +0100

    add class ProgramUsage

---
+++ base/ProgramUsage.kt
@@ -0,0 +1,31 @@
+import java.util.concurrent.TimeUnit.*
+
+class ProgramUsage {
+    private var startTime: Long = 0
+    private var endTime: Long = 0
+
+    fun start() {
+        startTime = System.currentTimeMillis()
+    }
+
+    fun stop() {
+        endTime = System.currentTimeMillis()
+    }
+
+    fun getElapsedTime(): String {
+        val elapsedTime = endTime - startTime
+        val hours = MILLISECONDS.toHours(elapsedTime)
+        val minutes = MILLISECONDS.toMinutes(elapsedTime) % 60
+        val seconds = MILLISECONDS.toSeconds(elapsedTime) % 60
+
+        val result = if (hours > 0) {
+            String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        } else if (minutes > 0) {
+            String.format("%02d:%02d", minutes, seconds)
+        } else {
+            String.format("%02d", seconds)
+        }
+
+        return result
+    }
+}
